6152f0a9955edbe242be70ef0f5a990ad8eb2cf7
parthadroja247 <parthadroja247@gmail.com>
2018-10-10 18:05:19 +0530

    Added Icon Added launch screen

---
+++ MobiWeather/MobiWeather/Customs/Controllers/MobiBaseViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MobiBaseViewController.swift
+//  MobiWeather
+//
+//  Created by Parth Adroja on 10/10/18.
+//  Copyright © 2018 Parth Adroja. All rights reserved.
+//
+
+import UIKit
+
+class MobiBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
